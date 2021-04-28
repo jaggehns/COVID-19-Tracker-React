1403618cc98b46007c96661f2797898cdc360b75
Jaggehn <jaggehns@gmail.com>
2021-04-28 17:57:53 +0800

    Revert "Implemented LineGrpah component"
    This reverts commit 24cd92cb92842b7dd7be91d83b4844bfd78ddd0f.

--- src/App.js
+++ src/App.js
@@ -11,7 +11,6 @@ import Map from "./Map";
 import Table from "./Table";
 import { sortData } from "./utils";
 import "./App.css";
-import LineGraph from "./LineGraph";
 
 function App() {
   const [countries, setCountries] = useState([]);
@@ -125,7 +124,6 @@ function App() {
           <h3>Live Cases by Country</h3>
           <Table countries={tableData} />
           <h3>Worldwide new cases</h3>
-          <LineGraph />
           {/*Graph*/}
         </CardContent>
       </Card>
